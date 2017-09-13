c45ac87b4daf3eec7e2c1d2ec768874a82302749
Samuel Ryzycki <samuel@ryzycki.com>
2017-09-13 18:06:35 +0100

    feat(button): emit “clicked” event
    More and more functionality in this amazing ButtonModule!

--- src/app/modules/button/button.component.ts
+++ src/app/modules/button/button.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-button',
   styleUrls: ['./button.component.scss'],
   template: `
-    <button class="pretty-button">
+    <button
+      class="pretty-button"
+      (click)="clicked.emit('Ive been so clicked!')"
+    >
       <ng-content></ng-content>
     </button>
   `
 })
 export class ButtonComponent implements OnInit {
+  /**
+   * Emits a string when the button is clicked.
+   */
+  @Output()
+  public clicked: EventEmitter<string> = new EventEmitter<string>();
 
   constructor() { }
 
