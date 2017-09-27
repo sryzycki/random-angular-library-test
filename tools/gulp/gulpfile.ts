a0434541eee1b09e4fcced84e260b91d84b4dfe7
Samuel Ryzycki <samuel@ryzycki.com>
2017-09-27 15:36:54 +0100

    feat(build): ensure _theming.scss content is deduped

--- tools/gulp/gulpfile.ts
+++ tools/gulp/gulpfile.ts
@@ -1,18 +1,28 @@
-import { dest, src, task } from 'gulp';
+import { task } from 'gulp';
 import { resolve } from 'path';
-import { mkdirpSync, writeFileSync } from 'fs-extra';
+import { writeFileSync } from 'fs-extra';
 import { Bundler } from 'scss-bundle';
 
 // The entry-point for the scss theming bundle.
 const themingEntryPointPath = resolve('./src/styles/theming/_all-theme.scss');
 // Output path for the scss theming bundle.
 const themingBundlePath = resolve('./dist/_theming.scss');
-// Matches all SCSS files in the library.
-const allScssGlob = resolve('./src/styles/**/*.scss');
+// Matches all SCSS files that are inside of Angular modules.
+const allModulesScssGlob = resolve('./src/app/modules/**/*.scss');
+// Matches all SCSS files to do with theming/typography Sass functions/mixins.
+const allThemingScssGlob = resolve('./src/styles/**/*.scss');
 
-
-task('bundleThemeScss', function (done) {
-  return new Bundler().Bundle(themingEntryPointPath, [allScssGlob])
+/**
+ * Bundle all the theming/typography SCSS code.
+ *
+ * So there are two types of SCSS code in this project:
+ * 1) non theme related SCSS that is handled by Angular components,
+ * 2) theme/typography SCSS that needs to be bundled separately and
+ *    exposed inside of the /dist folder.
+ */
+task('theming:bundle', function () {
+  return new Bundler()
+    .Bundle(themingEntryPointPath, [allModulesScssGlob, allThemingScssGlob])
     .then(result => {
       writeFileSync(themingBundlePath, result.bundledContent);
     });
