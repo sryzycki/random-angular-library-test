503d38637b78c0a2512dbe9826932eae82f067a1
Samuel Ryzycki <samuel@ryzycki.com>
2017-10-06 11:37:03 +0100

    feat(app): wrap RbxButtonModule showcase with rbx-card element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RbxCardModule } from './modules/card/card.module';
 import { RbxButtonModule } from './modules/button/button.module';
 
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    RbxCardModule,
     RbxButtonModule,
   ],
   providers: [],
